2c1bfe0f955f5aa09569b67f6344437cf6db26ce
Hafiz Ahmad Hassan <31056702+HafizAhmadHassan@users.noreply.github.com>
2021-03-13 18:51:21 +0100

    Logistic

---
+++ Course1/LogisticRegression/driver.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from Course1.LogisticRegression.lr_utils import *
+
+from Course1.LogisticRegression.fun import *
+
+if __name__ == '__main__':
+    # Load Data set
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+
+    train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+    print("X")
+    index = 25
+    plt.imshow(train_set_x_orig[index])
+    print("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode(
+        "utf-8") + "' picture.")
+
+    # checking Dataset Dimension
+
+    m_train = train_set_x_orig.shape[0]
+    m_test = test_set_x_orig.shape[0]
+    num_px = test_set_x_orig[0].shape[0]
+
+    print("Number of training examples: m_train = " + str(m_train))
+    print("Number of testing examples: m_test = " + str(m_test))
+    print("Height/Width of each image: num_px = " + str(num_px))
+    print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+    print("train_set_x shape: " + str(train_set_x_orig.shape))
+    print("train_set_y shape: " + str(train_set_y.shape))
+    print("test_set_x shape: " + str(test_set_x_orig.shape))
+    print("test_set_y shape: " + str(test_set_y.shape))
+
+    # Reshape the training and test examples
+
+
+
+
+
+    train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+
+    print("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+    print("train_set_y shape: " + str(train_set_y.shape))
+    print("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+    print("test_set_y shape: " + str(test_set_y.shape))
+    print("sanity check after reshaping: " + str(train_set_x_flatten[0:5, 0]))
+
+
+
+
+    #Normalize
+
+    train_set_x = train_set_x_flatten / 255.
+    test_set_x = test_set_x_flatten / 255.
+
+    d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=2000, learning_rate=0.005,
+              print_cost=True)
+
+    # Plot learning curve (with costs)
+    costs = np.squeeze(d['costs'])
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(d["learning_rate"]))
+    plt.show()
+
+
+    #Choosing different Learning Rate
+    learning_rates = [0.01, 0.001, 0.0001]
+    models = {}
+    for i in learning_rates:
+        print("learning rate is: " + str(i))
+        models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=1500, learning_rate=i,
+                               print_cost=False)
+        print('\n' + "-------------------------------------------------------" + '\n')
+
+    for i in learning_rates:
+        plt.plot(np.squeeze(models[str(i)]["costs"]), label=str(models[str(i)]["learning_rate"]))
+
+    plt.ylabel('cost')
+    plt.xlabel('iterations (hundreds)')
+
+    legend = plt.legend(loc='upper center', shadow=True)
+    frame = legend.get_frame()
+    frame.set_facecolor('0.90')
+    plt.show()
